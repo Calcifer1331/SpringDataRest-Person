440c50eae337e4265f475d4f4c5949d3d0424257
Yoseph Araúz <154104882+Calcifer1331@users.noreply.github.com>
2024-12-03 15:07:22 -0500

    Initial commit

---
+++ src/main/java/com/github/calcifer1331/springdatarestperson/SpringDataRestPersonApplication.java
@@ -0,0 +1,13 @@
+package com.github.calcifer1331.springdatarestperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestPersonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRestPersonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/calcifer1331/springdatarestperson/SpringDataRestPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.calcifer1331.springdatarestperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestPersonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
